0bb2dc4b3efcd4e752bdcedd764590bd9627b325
Francesco Guarino <franc@MacBook-Pro-di-Francesco.local>
2025-05-12 10:43:35 +0200

    ADD : configurbuilding , centralizzazione della logica di configurazione

---
+++ src/utils/ConfigBuilder.js
@@ -0,0 +1,51 @@
+import path from 'path';
+import os from 'os';
+
+export class ConfigBuilder {
+  constructor(baseConfig = {}) {
+    this.config = { ...baseConfig };
+  }
+
+  setNodeId(nodeId) {
+    this.config.node = this.config.node || {};
+    this.config.node.id = nodeId;
+    return this;
+  }
+
+  setNodeName(name) {
+    this.config.node = this.config.node || {};
+    this.config.node.name = name;
+    return this;
+  }
+
+  setDataDir(dataDir) {
+    this.config.node = this.config.node || {};
+    this.config.node.dataDir = dataDir;
+    this.config.storage = this.config.storage || {};
+    this.config.storage.path = path.join(dataDir, 'data');
+    return this;
+  }
+
+  setP2PPort(port) {
+    this.config.p2p = this.config.p2p || {};
+    this.config.p2p.port = port;
+    return this;
+  }
+
+  enablePersistentPeerId(peerId) {
+    this.config.p2p = this.config.p2p || {};
+    this.config.p2p.persistentPeerId = true;
+    this.config.p2p.savedPeerId = peerId;
+    return this;
+  }
+
+//   setApiPort(port) {
+//     this.config.api = this.config.api || {};
+//     this.config.api.port = port;
+//     return this;
+//   }
+
+  build() {
+    return this.config;
+  }
+}
